bd66987edb58e64ab25542beb48ea41988f69ca6
DeepakG123 <goel.deepak9@gmail.com>
2018-06-04 17:11:15 -0400

    Final App

--- src/App.js
+++ src/App.js
@@ -2,33 +2,71 @@ import React, { Component } from 'react';
 import './App.css';
 import Form from './Form.js'; 
 import Display from './Display.js'; 
+import firebase from 'firebase'; 
+import Button from '@material-ui/core/Button'; 
 
 let contracts_temp = []; 
 
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyB-ywU1pkPHmvWC9f_eW6RQjRMCyr-ZoTY",
+  authDomain: "contract-database.firebaseapp.com",
+  databaseURL: "https://contract-database.firebaseio.com",
+  projectId: "contract-database",
+  storageBucket: "contract-database.appspot.com",
+  messagingSenderId: "205529720817"
+};
+ firebase.initializeApp(config);
+
+ var database = firebase.database();
+
+
+
+
 class App extends Component {
+
+
   constructor(props) {
     super(props);
     this.state = {
-      contracts: [] 
+      contracts: [], 
+      refs: []
   }
 }
 
 updateField(newValue ) {
-  contracts_temp.push([newValue.name,newValue.company, newValue.contract ])
-  console.log(contracts_temp)
-  this.setState({
-      contracts: contracts_temp
-  }
-  )
+  var newContract = firebase.database().ref().push(); 
+  newContract.set({
+    name: newValue.name, 
+    company: newValue.company, 
+    contract: newValue.contract 
+  }); 
 }
 
+componentWillMount(){
+  firebase.database().ref().on("value", (snapshot) => {
+    let contracts1 = []; 
+    let refs1 = []; 
+    snapshot.forEach(function(data){
+      refs1.push(data.key)
+      contracts1.push(data.val()); 
+    }); 
+    this.setState({
+      contracts: contracts1, 
+      refs: refs1
+    })
+})
+}
+
+
   render() {
-    console.log(this.state.contracts)
+    console.log(this.state)
     return (
       <div className="App">
         <Form
         updateParent  = {newValue => this.updateField(newValue)}
         /> 
+        <br/> 
         <Display
         contracts = {this.state.contracts}
         /> 
--- src/Display.js
+++ src/Display.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react'; 
+import firebase from 'firebase'; 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { common } from '@material-ui/core/colors';
+
+ // Initialize Firebase
 
 class Display extends Component {
 
-    render() {
 
+    render() {
         console.log(this.props.contracts)
         return (
             <div> 
-           {this.props.contracts.map(contract => 
-           <div> 
-           <p> {contract[0]} </p>
-           <p> {contract[1]} </p>
-           <p> {contract[2]} </p>
-           </div>)}    
+             <Paper>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Company</TableCell>
+            <TableCell>Contract</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {this.props.contracts.map(contract => {
+            return (
+              <TableRow>
+                <TableCell>{contract.name}</TableCell>
+                <TableCell>{contract.company}</TableCell>
+                <TableCell>{contract.contract}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
             </div> 
         ); 
     }
